d3554e9266247b80db44f64480fdc43dd8780426
Bhuvi100 <bhuvi@kajuzi.co.za>
2021-08-01 14:18:22 +0530

    Fix signout from dashboard

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 module.exports = {
-  reactStrictMode: false,
-  images: {
-      loader: 'imgix',
-  },
+    reactStrictMode: false,
+    images: {
+        loader: 'imgix',
+    },
     eslint: {
         dirs: [],
     },
-
+    async redirects() {
+        return [
+            {
+                source: '/dashboard/signout',
+                destination: '/signout',
+                permanent: true,
+            },
+        ]
+    },
 }
