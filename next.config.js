5a6078da331cce4cd14e47b6530e23f09bc61862
Bhuvi100 <bhuvi@kajuzi.co.za>
2021-11-19 12:13:46 +0530

    Fix next config

--- next.config.js
+++ next.config.js
@@ -1,33 +1,6 @@
 module.exports = {
     reactStrictMode: false,
-    images: {
-        loader: 'imgix',
-    },
     eslint: {
         dirs: [],
-    },
-    async redirects() {
-        return [
-            {
-                source: '/dashboard/signout',
-                destination: '/signout',
-                permanent: true,
-            },
-            {
-                source: '/dashboard/dashboard/signout',
-                destination: '/signout',
-                permanent: true,
-            },
-            {
-                source: '/register',
-                destination: '/signup',
-                permanent: true,
-            },
-            {
-                source: '/signin',
-                destination: '/login',
-                permanent: true,
-            },
-        ]
-    },
+    }
 }
