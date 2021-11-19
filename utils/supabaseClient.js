93c7e53c8c7e0699e94f7419b1a1a4b004d45db2
Bhuvi100 <bhuvi@kajuzi.co.za>
2021-11-19 11:56:30 +0530

    Use env variables to initialize supabase client

--- utils/supabaseClient.js
+++ utils/supabaseClient.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 export const supabase = createClient(
-  "https://lcgeprvftwnteheqsucd.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNzQ1NTgxMCwiZXhwIjoxOTQzMDMxODEwfQ.kCy-q15D65c4_qegrTRTh9Qif-cnYkdg-mSmzZ8SROE"
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
