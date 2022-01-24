7a271ef4c330e81f14eff5cd418b983996dccbb5
Bhuvi100 <bhuvi@kajuzi.co.za>
2022-01-25 01:36:49 +0530

    Move signin event listener to useEffect in index page

--- pages/index.js
+++ pages/index.js
@@ -8,41 +8,39 @@ import Head from "next/head";
 import { Spacer } from "@chakra-ui/react";
 import Footer from "../components/Footer/Footer";
 import MembersMarquee from "../components/MembersMarquee/MembersMarquee";
-import {useLayoutEffect, useState} from "react";
-import {supabase} from "../utils/supabaseClient";
+import { useEffect, useState } from "react";
+import { supabase } from "../utils/supabaseClient";
 import Router from "next/router";
 
 export default function Home(props) {
-  const [dataFetched, setDataFetched] = useState(false)
-  const [members, setMembers] = useState(null)
+  const [dataFetched, setDataFetched] = useState(false);
+  const [members, setMembers] = useState(null);
 
-  useLayoutEffect (() => {
-    async function fetchData() {
-      const {data, error} = await supabase
-          .from('profiles')
-          .select('name, avatar_url, priority')
-          .order('priority', { ascending: false })
+  async function fetchData() {
+    const { data, error } = await supabase
+        .from("profiles")
+        .select("name, avatar_url, priority")
+        .order("priority", { ascending: false });
 
-      if (error) {
-        throw error
-      } else {
-          setMembers(data)
-        }
+    if (error) {
+      throw error;
+    } else {
+      setMembers(data);
     }
+  }
+
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((event, session) => {
+      if (event === "SIGNED_IN") {
+        Router.push("/dashboard");
+      }
+    });
 
     if (!dataFetched) {
-      setDataFetched(true)
-      fetchData()
+      setDataFetched(true);
+      fetchData();
     }
-
-  }, [dataFetched])
-
-  supabase.auth.onAuthStateChange((event, session) => {
-    if (event === 'SIGNED_IN') {
-      Router.push('/dashboard')
-    }
-  })
-
+  }, [dataFetched]);
 
   return (
     <div>
@@ -55,23 +53,27 @@ export default function Home(props) {
           key="title"
         />
       </Head>
-    <ChakraProvider>
-      <Navbar loggedIn={props.loggedIn} />
-      <Hero loggedIn={props.loggedIn}/>
-      <Spacer />
-      <a name="about" id="about">
-        <About p={20} />
-      </a>
-      <Divider />
-      <a name="events" id="events">
-        <Events />
-      </a>
-      <Divider />
-      {dataFetched === true && members !== null ? <MembersMarquee members={members} /> : ''}
-      <Divider />
-      <Sponsors />
-      <Footer p={20} />
-    </ChakraProvider>
+      <ChakraProvider>
+        <Navbar loggedIn={props.loggedIn} />
+        <Hero loggedIn={props.loggedIn} />
+        <Spacer />
+        <a name="about" id="about">
+          <About p={20} />
+        </a>
+        <Divider />
+        <a name="events" id="events">
+          <Events />
+        </a>
+        <Divider />
+        {dataFetched === true && members !== null ? (
+          <MembersMarquee members={members} />
+        ) : (
+          ""
+        )}
+        <Divider />
+        <Sponsors />
+        <Footer p={20} />
+      </ChakraProvider>
     </div>
   );
 }
