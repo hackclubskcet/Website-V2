d2fda4280602c0bfe43dc2d3db002a59132b6387
Bhuvi100 <bhuvi@kajuzi.co.za>
2022-05-09 19:41:40 +0530

    Add bootcode link

---
+++ pages/bootcode.js
@@ -0,0 +1,9 @@
+import {useEffect} from "react";
+
+export default function Bootcode() {
+    useEffect(() => {
+        window.location.href = 'https://hackclub.zoom.us/j/87469734121'
+    })
+
+    return <div>Redirecting.......</div>
+}
