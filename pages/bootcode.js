04a7b1b4a7d4ffd650ed8421210cf8a5d162e823
Bhuvi100 <81316368+Bhuvi100@users.noreply.github.com>
2022-05-11 20:22:22 +0530

    Update new Bootcode zoom link

--- pages/bootcode.js
+++ pages/bootcode.js
@@ -2,7 +2,7 @@ import {useEffect} from "react";
 
 export default function Bootcode() {
     useEffect(() => {
-        window.location.href = 'https://www.google.com/url?q=https://hack.af/z-join?id%3Dxdnv9&sa=D&source=calendar&ust=1652534760457935&usg=AOvVaw2_u4okLsgS4q0XeuaMp9di'
+        window.location.href = 'https://hack.af/z-join?id=ujmder'
     })
 
     return <div>Redirecting.......</div>
