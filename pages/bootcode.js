8a780c0e98d211eb0802e63f3ecb293026565ccb
Bhuvi100 <81316368+Bhuvi100@users.noreply.github.com>
2022-05-14 20:16:39 +0530

    Update Bootcode link

--- pages/bootcode.js
+++ pages/bootcode.js
@@ -2,7 +2,7 @@ import {useEffect} from "react";
 
 export default function Bootcode() {
     useEffect(() => {
-        window.location.href = 'https://hack.af/z-join?id=ujmder'
+        window.location.href = 'https://hackclub.zoom.us/j/82222505071?pwd=RjdtWU90VkdBckNCMXl1VjdkVFl4dz09'
     })
 
     return <div>Redirecting.......</div>
